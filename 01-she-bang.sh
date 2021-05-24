762baefc6e4cb589649ef655187e9a3a67478635
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-05-24 19:23:23 +0530

    writting the data to this she-bang.sh

---
+++ 01-she-bang.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#! is called as she-bang.
+# 1. It is called as she-bang,
+# 1. It is used to denote the path of interpreter.
+#2. It has to be in  the line number 1.
+
+
+ls
+find /etc -name passwd
