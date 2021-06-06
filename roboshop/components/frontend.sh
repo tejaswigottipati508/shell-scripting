d268824e8b0b0d8849d9a12b06ba4fabb06975b1
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-07 00:04:44 +0530

    writing data in frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,15 +4,18 @@ COMPONENT=frontend
 
 source components/common.sh
 
-print "Installing Nginx" "yum install nginx -y"
-yum install nginx -y &>/tmp/log
+Print "Installing Nginx" "yum install nginx -y"
+yum install nginx -y
 Stat $?
 
-print "Starting Nginx Service" "systemctl start nginx"
-exit
+Print "Starting Nginx Service" "systemctl start nginx"
 systemctl enable nginx
 systemctl start nginx
+Stat $?
+
+Print "Downloading Frontend Content" 'curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Stat $?
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
