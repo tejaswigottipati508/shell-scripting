ea0f8090be53ac30a4687eb725235a7f921f81e5
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-07 01:22:55 +0530

    writing data

---
+++ roboshop/Create-instance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+component=$1
+
+if [ -z "${component}" ]; then
+  echo "Need a Input of Component Name"
+  exit 1
+fi
+
+ aws ec2 run-instances --launch-template LaunchTemplateId=lt-08658ca63d7181d6b --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${component}}]"
