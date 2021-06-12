30afe112debc41543ee4d8814267898595cb0a03
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-12 16:03:32 +0530

    writing data

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -3,9 +3,9 @@
 USER_ID=$(id -u)
 
 if [ "$USER_ID" -ne 0 ]; then
-  echo -e "\e[31mYou must be a root user to execute this script\e[0m"
+  echo -e "\e[1;31mYou must be a root user to execute this script\e[0m"
   exit 1
-  fi
+fi
 #Linux os Based
 #print() {
 #  echo -e "\e[1m $(date +%c) \e[35m$(hostname)\e[0m \e[1;36m${COMPONENT}\e[0m ::$1"
@@ -16,17 +16,19 @@ print() {
    \e[0m>----------------------------------"
   echo -e "[\e[1;34mINFO\e[0m]\e[1m $2 \e[0m"
   echo -e "[\e[1;
-  34mINFO\e[0m]--------------------------------------------------------------------\n"
+  34mINFO\e[0m
+  ]--------------------------------------------------------------------\n"
+
 }
 
-Stat(){
+Stat() {
   echo -e "\n[\e[1;
     34mINFO\e[0m
-    ]-----------------------------------------"
+    ]--------------------------------------------------------"
   if [ "$1" -eq 0  ]; then
     echo -e "[\e[1;34mINFO\e[0m] \e[1;32mSUCCESS \e[0m"
   else
-    echo -e "[\e[1;34mINFO\e[0m] \e[1;31m EXIT STATUS - $1 :: FAILURE \e[0m"
+    echo -e "[\e[1;34mINFO\e[0m] \e[1;31mFAILURE \e[0m"
     exit 2
   fi
  echo -e "[\e[1;
