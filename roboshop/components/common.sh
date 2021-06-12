321b26060f76b946f5a2e0d1cfab0c78a82eb102
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-12 16:15:39 +0530

    writing data

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -6,32 +6,9 @@ if [ "$USER_ID" -ne 0 ]; then
   echo -e "\e[1;31mYou must be a root user to execute this script\e[0m"
   exit 1
 fi
-#Linux os Based
-#print() {
-#  echo -e "\e[1m $(date +%c) \e[35m$(hostname)\e[0m \e[1;36m${COMPONENT}\e[0m ::$1"
-#}
 
 print() {
-  echo -e "[\e[1;34mINFO\e[0m]---------------< \e[1m $1
-   \e[0m>----------------------------------"
-  echo -e "[\e[1;34mINFO\e[0m]\e[1m $2 \e[0m"
-  echo -e "[\e[1;
-  34mINFO\e[0m
-  ]--------------------------------------------------------------------\n"
-
+  echo -e "\e[1m $(date +%c) \e[35m$(hostname)\e[0m \e[1;36m${COMPONENT} :: $1"
 }
 
-Stat() {
-  echo -e "\n[\e[1;
-    34mINFO\e[0m
-    ]--------------------------------------------------------"
-  if [ "$1" -eq 0  ]; then
-    echo -e "[\e[1;34mINFO\e[0m] \e[1;32mSUCCESS \e[0m"
-  else
-    echo -e "[\e[1;34mINFO\e[0m] \e[1;31mFAILURE \e[0m"
-    exit 2
-  fi
- echo -e "[\e[1;
- 34mINFO\e[0m
- ]----------------------------------------------\n"
-}
+
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,25 +4,18 @@ COMPONENT=frontend
 
 source components/common.sh
 
-Print "Installing Nginx" "yum install nginx -y"
+Print "Installing Nginx"
 yum install nginx -y
-Stat $?
 
-Print "Starting Nginx Service" "systemctl start nginx"
+Print "Starting Nginx Service"
+exit
 systemctl enable nginx
 systemctl start nginx
-Stat $?
-
-
-Print "Downloading Frontend Content" 'curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-Stat $?
-
-
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip  mv frontend-main/* . && mv static/* . && rm -rf frontend-master README.md
+unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
