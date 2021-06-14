1e549cc430bbaa949e2f627c4f34ae9b89595ea3
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-14 18:54:35 +0530

    writting data to files

--- roboshop/Create-instance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-component=$1
-if [ -z "${component}" ]; then
-  echo "Need a Input of Component Name"
-  exit 1
-fi
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-lt-005d5ab8348b23745 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
