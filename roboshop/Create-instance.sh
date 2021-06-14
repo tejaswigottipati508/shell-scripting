177d54775197d37ddf2c21813af51fccc099d73a
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-14 14:05:56 +0530

    writing data to files

--- roboshop/Create-instance.sh
+++ roboshop/Create-instance.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-component=$1
-
+component=$1case $x in
 if [ -z "${component}" ]; then
   echo "Need a Input of Component Name"
   exit 1
 fi
 
-STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" --query 'Reservations[*].Instances[*].State.Name' --OUTPUT TEXT)
-
-if [ STATE != "running" ]; then
-  aws ec2 run-instances --launch-template LaunchTemplateId=lt-08658ca63d7181d6b --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${component}}]"
-fi
-
-$ aws route53 change-resource-record-sets --hosted-zone-id --change-batch file://sample.json
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-lt-005d5ab8348b23745 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
