d06774a79ad6333b413672ed3f4238e129b47135
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-14 21:34:30 +0530

    writting data to files

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo I am common
+##Validate the script is executed as root user or not.
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -NE 0]; then
+  echo -e "\e[31mYou must be a root user to execute this script\e[0m"
+fi
+
