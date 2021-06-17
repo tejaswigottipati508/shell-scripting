eed975402890bf77dfad66b72c28dc15c88b8344
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-17 23:34:55 +0530

    writting data to files

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -10,7 +10,7 @@ STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}"
 
 
 if [ "$STATE" != "running" ]; then
-  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-lt-lt-02b2e635877a68607 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-02b2e635877a68607 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
