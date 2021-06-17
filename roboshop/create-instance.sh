98a4e2a233c046755292b38a018ec3ab7b698b5e
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-18 00:30:49 +0530

    writting data to files

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -10,7 +10,7 @@ fi
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
 if [ "$STATE" != "running" ]; then
-  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-lt-02b2e635877a68607 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq .
+  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-02b2e635877a68607 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq .
   sleep 15
 fi
 
