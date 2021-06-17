7fc8083a719960a34c247c6fe9e5dc1e35166249
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-18 00:18:12 +0530

    writting data to files

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -10,15 +10,15 @@ fi
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name')
 
 
-if [ "$STATE" != "running" ]; then
+if [ $STATE != "running" ]; then
   aws ec2 run-instances  --launch-template LaunchTemplateId=lt-02b2e635877a68607 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
 export IPADDRESS
 
-envsubst <record.json >/tmp/"${component}".json
+envsubst <record.json >/tmp/${component}.json
 
-aws route53 change-resource-record-sets --hosted-zone-id Z05871891T73BLRZZ77ZS --change-batch file:///tmp/"${component}".json
+aws route53 change-resource-record-sets --hosted-zone-id Z05871891T73BLRZZ77ZS --change-batch file:///tmp/${component}.json
 
 
