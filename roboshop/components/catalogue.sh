0ed9841893f21bdd4c3c5b199ce5419b0ed95a5b
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-18 12:38:22 +0530

    writting data to files

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -26,7 +26,7 @@ npm install --unsafe-perm
 Stat $?
 
 Print "Update SystemD Script for Catalogue" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.tejadevops.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service"
-sed -i -e 's/MONGO_DNSNAME/mongodb-ss.tejadevops.tk/' /home/roboshop/catalogue/systemd.service &&  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+sed -i -e 's/MONGO_DNSNAME/mongodb-ss.tejadevops.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 Stat $?
 
 Print "Start Catalogue Service" "systemctl daemon-reload && systemctl restart catalogue && systemctl enable catalogue"
