4429457958d908b383288779b26cd5a74e84623c
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-20 18:43:11 +0530

    writting data to files

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -21,8 +21,9 @@ Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && mk
 rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue  && cd /home/roboshop/catalogue
 Stat $?
 
-exit
-$ npm install
+Print "Install NODEJS Dependencies" "npm install"
+npm install
+Stat $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
