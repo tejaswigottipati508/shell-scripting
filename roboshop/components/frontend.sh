336c599981174f440b84f4259b225748a517981b
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-22 21:33:24 +0530

    writting data to files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,29 +6,18 @@ source components/common.sh
 
 Print "Installing Nginx" "yum install nginx -y"
 yum install nginx -y
-Stat $?
-
-
-Print "Downloading Frontend Content" 'curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
-
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-Stat $?
-
-Print "Remove Old Docs" "cd /usr/share/nginx/html ;rm -rf *"
-cd /usr/share/nginx/html
-rm -rf *
-Stat $?
-
-Print "Extract RoboShop Frontend Docs" "unzip /tmp/frontend.zip"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-master README.md
-Stat $?
-
-Print "Setup Nginx Config for RoboShop" "mv localhost.conf /etc/nginx/default.d/roboshop.conf"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-Stat $?
-
 
 Print "Starting Nginx Service" "systemctl start nginx"
+exit
 systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master README.md
+Print "Setup Nginx Config for RoboShop"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
-Stat $?
