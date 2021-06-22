16d27003a94346e9b627a80c00968091424f47bd
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-22 21:44:07 +0530

    writting data to files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,18 +6,24 @@ source components/common.sh
 
 Print "Installing Nginx" "yum install nginx -y"
 yum install nginx -y
+Stat $?
 
 Print "Starting Nginx Service" "systemctl start nginx"
 exit
 systemctl enable nginx
 systemctl start nginx
+Stat $?
+
+Print "Downloading Frontend Content" 'curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
+
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Stat $?
+
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
-Print "Setup Nginx Config for RoboShop"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
