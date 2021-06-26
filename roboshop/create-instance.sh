6b030d91efa531f4a577f55d7d13ecfc76b69b3a
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-26 21:53:38 +0530

    writting data to files

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -18,4 +18,4 @@ export IPADDRESS
 
 envsubst <record.json >/tmp/${component}.json
 
-#aws route53 change-resource-record-sets --hosted-zone-id Z05871891T73BLRZZ77ZS --change-batch file:///tmp/${component}.json
+aws route53 change-resource-record-sets --hosted-zone-id Z05871891T73BLRZZ77ZS --change-batch file:///tmp/${component}.json
