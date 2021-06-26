bb555ecdcfe308ab313a22001f8df60a41fe5a3a
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-26 19:47:29 +0530

    writting data to files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,21 +8,25 @@ Print "Installing Nginx" "yum install nginx -y"
 yum install nginx -y
 Stat $?
 
-Print "Starting Nginx Service" "systemctl start nginx"
-systemctl enable nginx
-systemctl start nginx
-Stat $?
-
 Print "Downloading Frontend Content" 'curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"'
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 Stat $?
 
+Print "Remove Old Docs" "cd /usr/share/nginx/html"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master README.md
+Stat $?
+
+Print "Extract RoboShop Frontend Docs" "unzip /tmp/frontend.zip"
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-master README.md
+Stat $?
+
+Print "Setup Nginx Config for RoboShop" "mv localhost.conf /etc/nginx/default.d/roboshop.conf"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Stat $?
+
+Print "Starting Nginx Service" "systemctl start nginx"
+systemctl enable nginx
 systemctl restart nginx
+Stat $?
