9e6203025423a9b83f3083af64c6ede4243fd8bc
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-27 22:15:15 +0530

    writting data to files

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -14,21 +14,19 @@ Stat $?
 
 Print "Download Cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"'
 
-curl -s -L -o /tmp/cart.zip https://github.com/roboshop-devops-project/cart/archive/main.zip
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 Stat $?
 
-Print  "Extract Cart Component Code" "rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd /home/roboshop/cart"
-rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd /home/roboshop/cart
+Print  "Extract Cart Component Code"  "cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd cart"
+cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd cart
 Stat $?
 
 Print "Install NODEJS Dependencies" "npm install"
 npm install --unsafe-perm
 Stat $?
 
-chown roboshop:roboshop /home/roboshop -R
-
-Print "Update SystemD Script for Cart" "sed -i -e 's/MONGODB_ENDPOINT/mongodb-ss.tejadevops.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service"
-sed -i  -e 's/REDIS_ENDPOINT/redis-ss.tejadevops.tk/'  /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+Print "Update SystemD Script for Cart" "sed -i -e 's/MONGODB_DNSNAME/mongodb-ss.tejadevops.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service"
+sed -i  -e 's/MONGODB_DNSNAME/mongodb-ss.tejadevops.tk/'  /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 Stat $?
 
 Print "Start Cart Service" "systemctl daemon-reload && systemctl restart cart && systemctl enable cart"
--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -17,8 +17,8 @@ Print "Download Catalogue Component Code" 'curl -s -L -o /tmp/catalogue.zip "htt
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 Stat $?
 
-Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
-rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
+Print  "Extract Catalogue Component Code" "cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
+cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
 Stat $?
 
 Print "Install NodeJs Dependencies" "npm install"
