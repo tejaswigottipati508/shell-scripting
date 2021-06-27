504bbadcb8c0957fc70310b70b4c8d71b6bfb539
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-06-27 21:11:29 +0530

    writting data to files

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+COMPONENT=catalogue
+
+source components/common.sh
+
+Print "Installing NodeJS" "yum install nodejs make gcc-c++ -y"
+yum install nodejs make gcc-c++ -y
+Stat $?
+
+Print "Adding RoboShop Project User" "useradd roboshop"
+useradd roboshop
+Stat $?
+
+Print "Download Catalogue Component Code" 'curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"'
+
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Stat $?
+
+Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
+rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
+Stat $?
+
+exit
+$ npm install
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
