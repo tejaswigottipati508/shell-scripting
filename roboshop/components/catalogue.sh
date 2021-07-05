9c27849f0b3380a2d33480f15cd64369bfb296da
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-07-05 19:28:59 +0530

    writting data to files

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -17,20 +17,13 @@ Print "Download Catalogue Component Code" 'curl -s -L -o /tmp/catalogue.zip "htt
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 Stat $?
 
-Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue"
-rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
+Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip"
+rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip
 Stat $?
+exit
+$ npm install
 
-Print "Install NOdeJS Dependencies" "npm install"
-npm install --unsafe-perm
-Stat $?
-
-chown roboshop:roboshop /home/roboshop -R
-
-Print "Update SystemD Script for Catalogue" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.tejadevops.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service"
-sed -i -e 's/MONGO_ENDPOINT/mongodb-ss.tejadevops.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-Stat $?
-
-Print "Start Catalogue Service" "systemctl daemon-reload && systemctl restart catalogue && systemctl enable catalogue"
-systemctl daemon-reload && systemctl restart catalogue && systemctl enable catalogue
-Stat $?
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
