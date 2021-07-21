faff1fa6d668e99f5f6ba1ba1bc194f412ad81ca
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-07-21 19:37:57 +0530

    writting data to files

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -28,7 +28,8 @@ Stat $?
 echo DEFAULT_PASSWORD = $DEFAULT_PASSWORD
 
 Print "Reset MYSQL Password" ""
-mysql -uroot -p"${DEFAULT_PASSWORD}" <<EOF
+mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Default_RoboShop*999';
 uninstall plugin validate_password;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 EOF
