ca72df339e58f66410eb545bfd777573ed5c00fa
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-07-26 19:58:52 +0530

    writting data to files

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -17,8 +17,8 @@ Print "Download Shipping Code" 'curl -s -L -o /tmp/shipping.zip "https://github.
 curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
 Stat $?
 
-Print "Extract Shipping Code" "rm -rf /home/roboshop/shipping && unzip -O /tmp/shipping.zip && mv shipping-main shipping && cd shipping"
-rm -rf /home/roboshop/shipping && unzip  -O /tmp/shipping.zip && mv shipping-main shipping && cd shipping
+Print "Extract Shipping Code" "rm -rf /home/roboshop/shipping && unzip /tmp/shipping.zip && mv shipping-main shipping && cd shipping"
+rm -rf /home/roboshop/shipping && unzip /tmp/shipping.zip && mv shipping-main shipping && cd shipping
 Stat $?
 
 Print "Maven Compile Code" "mvn clean package && mv target/shipping-1.0.jar shipping.jar"
