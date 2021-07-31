187245239689b127ab10f2f3b7ebb0b5ba4432c3
Tejaswi G <gottipatitejaswi8@gmail.com>
2021-07-31 19:46:51 +0530

    writting data to files

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -23,14 +23,19 @@ Stat $?
 Print "Install Python Dependencies" "cd payment  && pip3 install -r requirements.txt"
 cd payment  && pip3 install -r requirements.txt
 Stat $?
-exit
 
-Update the roboshop user and group id in payment.ini file.
+Print "Update User details in Payment script" 'sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" /home/roboshop/payment/payment.ini'
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" /home/roboshop/payment/payment.ini
+Stat $?
 
-Setup the service
 chown roboshop :roboshop /home/roboshop -R
 
-# mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-# systemctl daemon-reload
-# systemctl enable payment
-# systemctl start payment
+Print "Update SystemD Script for Payment" 'sed -i -e "s/CARTHOST/cart-ss.tejadevops.tk/" -e "s/USERHOST/user-ss.tejadevops.tk/" -e "s/AMQPHOST/rabbitmq-ss.tejadevops.tk/" /home/roboshop/payment/systemd.service '
+sed -i -e "s/CARTHOST/cart-ss.tejadevops.tk/" -e "s/USERHOST/user-ss.tejadevops.tk/" -e "s/AMQPHOST/rabbitmq-ss.tejadevops.tk/" /home/roboshop/payment/systemd.service
+Stat $?
+
+Print "Start Payment Service" "mv /home/roboshop/payment/systemd.service  /etc/systemd/system/payment.service && systemctl daemon-reload && systemctl enable payment && systemctl start payment"
+mv /home/roboshop/payment/systemd.service  /etc/systemd/system/payment.service && systemctl daemon-reload && systemctl enable payment && systemctl start payment
+Stat $?
